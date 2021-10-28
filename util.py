8fc3597696cb2a89209c517a9a19dba4983d886e
Koorye <a1311586225@gmail.com>
2021-10-28 11:36:00 +0800

    Update util.py

--- util.py
+++ util.py
@@ -97,43 +97,4 @@ def load_models(historical_epochs, output_model_root, image_size):
     D_B.load_state_dict(torch.load(os.path.join(historical_root, 'D_B.pth')))
 
     return G_A2B, G_B2A, D_A ,D_B, last_epoch
-
-
-
-def load_models(historical_epochs, output_model_root, image_size):
-    """
-    读取模型
-    """
     
-    G_A2B = Generater().to(device)
-    G_B2A = Generater().to(device)
-    D_A = Discriminator(image_size).to(device)
-    D_B = Discriminator(image_size).to(device)
-
-    if historical_epochs == 0:
-        G_A2B.apply(weights_init)
-        G_B2A.apply(weights_init)
-        D_A.apply(weights_init)
-        D_B.apply(weights_init)
-        return G_A2B, G_B2A, D_A ,D_B, 0
-
-    if historical_epochs == -1:
-        paths = sorted(os.listdir(output_model_root))
-        last_epoch = 0
-        for path in paths:
-            if path.startswith('epoch'):
-                epoch = int(path[5:])
-                if epoch > last_epoch:
-                    last_epoch = epoch
-        historical_root = os.path.join(output_model_root, f'epoch{epoch}')
-    else:
-        last_epoch = historical_epochs
-        historical_root = os.path.join(output_model_root, f'epoch{historical_epochs}')
-
-    G_A2B.load_state_dict(torch.load(os.path.join(historical_root, 'G_A2B.pth')))
-    G_B2A.load_state_dict(torch.load(os.path.join(historical_root, 'G_B2A.pth')))
-    D_A.load_state_dict(torch.load(os.path.join(historical_root, 'D_A.pth')))
-    D_B.load_state_dict(torch.load(os.path.join(historical_root, 'D_B.pth')))
-
-    return G_A2B, G_B2A, D_A ,D_B, last_epoch
-
